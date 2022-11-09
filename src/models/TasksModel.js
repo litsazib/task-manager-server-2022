ca82adf8c8bfc337739b07c2906c9fa42ea538c0
litsazib <roktimsazib92@gmail.com>
2022-11-09 15:33:44 +0600

    remove lock

--- src/models/TasksModel.js
+++ src/models/TasksModel.js
@@ -2,6 +2,7 @@ const  mongoose=require('mongoose');
 const DataSchema=mongoose.Schema({
     title:{type:String},
     description:{type:String},
+    assigMember:{type:String},
     status:{type:String},
     email:{type:String},
     createdDate:{type:Date,default:Date.now()}
